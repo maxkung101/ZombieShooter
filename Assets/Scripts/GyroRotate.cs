46ba9576c7b1316390d46d10ef447ba8fb6b6445
Max Kung <maxkung06@gmail.com>
2021-07-08 20:16:53 -0400

    add GyroRotate.cs
    Support for mobile devices

---
+++ Assets/Scripts/GyroRotate.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroRotate : MonoBehaviour
+{
+    private Gyroscope gyro;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+        else
+        {
+            Debug.Log("Device does not support gyroscope.");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Gyroscope attitude : " + gyro.attitude);
+    }
+}
