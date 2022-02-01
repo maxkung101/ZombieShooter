b84d3d83fd5cc26944cde8f3acdfa14b80393d32
Max Kung <maxkung06@gmail.com>
2022-01-31 19:58:14 -0500

    update MainMenu.unity

--- Assets/Scripts/TextBehaviourMenu.cs
+++ Assets/Scripts/TextBehaviourMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextBehaviourMenu : MonoBehaviour
 {
@@ -9,11 +10,29 @@ public class TextBehaviourMenu : MonoBehaviour
     [TextArea]
     public string[] instructions, credits;
 
+    public TMP_Text lookAroundTMP, detailsTMP, highScoreTMP, settingsTMP, themeTMP, inGameMusicTMP, pointerTMP, languageTMP, resetScoreTMP, dawnTMP, dayTMP, sunsetTMP, nightTMP, autoTMP, playTMP;
+
     private int id;
 
     // Start is called before the first frame update
     private void Start()
     {
         id = PlayerPrefs.GetInt("VR Zombie Shooter Defender - SelectedLanguage", 0);
+
+        lookAroundTMP.text = lookAround[id];
+        detailsTMP.text = details[id];
+        highScoreTMP.text = highScore[id];
+        settingsTMP.text = settings[id];
+        themeTMP.text = theme[id];
+        inGameMusicTMP.text = inGameMusic[id];
+        pointerTMP.text = pointer[id];
+        languageTMP.text = language[id];
+        resetScoreTMP.text = resetScore[id];
+        dawnTMP.text = dawn[id];
+        dayTMP.text = day[id];
+        sunsetTMP.text = sunset[id];
+        nightTMP.text = night[id];
+        autoTMP.text = auto[id];
+        playTMP.text = play[id];
     }
 }
